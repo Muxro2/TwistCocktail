fc3fbcc1453643f9978e895a1fa92c3bae57c51b
Mo Roshid <167237482+Muxro2@users.noreply.github.com>
2025-11-04 01:24:20 +0000

    implimented landing page temporary

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,13 @@
 import { useState, useEffect } from "react"
 import Image from "next/image";
 import logo from "@/public/twistOutline.svg"
+import homepage from "@/public/homepage.png"
 import Searchbox from "@/components/Searchbox";
 import Card from "@/components/Card";
 import { fetchDrinks } from "@/lib/fetchDrinks";
 
 export default function Home() {
-  const [query, setQuery] = useState("Margarita");
+  const [query, setQuery] = useState("");
   const [drinks, setDrinks] = useState([]);
 
   const [searchFocused, setSearchFocused] = useState(false)
@@ -27,7 +28,7 @@ export default function Home() {
   <div className="w-full h-full">
 
     
-  <div className="fixed w-[100%] h-20 pt-3 flex justify-center bg-[#111] z-100">
+  <div className="fixed w-[100%] h-16 flex justify-center items-center bg-[#111] z-100">
   <div className={`relative h-[60%] transition-all duration-300 ease-in-out ${searchFocused ? "w-0" : "w-[120px]"}`}>
     <Image
     src={logo}
@@ -38,15 +39,26 @@ export default function Home() {
   </div>
   <Searchbox query={query} setQuery={setQuery} setSearchFocused={setSearchFocused}/>
   </div>
+  <div className="w-full h-16"></div>
+      {!query ?
+        <div className="relative w-full h-[90%]">
+          <Image
+            src={homepage}
+            alt="homepage"
+            fill
+            className="object-contain"
+          />
+        </div>
+       :
 
-  <div className="w-full h-20"></div>
+
   <div className="flex flex-col gap-1 overflow-hidden">
   {drinks.map((drink: any) => (
           <Card image={drink.strDrinkThumb} name={drink.strDrink} id={drink.idDrink} key={drink.idDrink}/>
   ))}
   </div>
-
-    
+  
+    }
   </div>
   );
 }
